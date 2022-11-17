15498b62c59de080bde46c0d9ebbffacbe618742
Ferdinand Linnenberg <fl@umh.app>
2022-11-17 13:35:16 +0100

    fix: incorrect param type

--- pkg/errwrap_test.go
+++ pkg/errwrap_test.go
@@ -22,7 +22,7 @@ func Test_ConstErrorWrapped(t *testing.T) {
 }
 
 func Test_ConstErrorParams(t *testing.T) {
-	params := map[string]string{"param1": "value1", "param2": "value2"}
+	params := map[string]any{"param1": "value1", "param2": "value2"}
 	err := exampleError.WithParams(params)
 	if err.Error() != "example error [{\"param1\":\"value1\",\"param2\":\"value2\"}]" {
 		t.Errorf("Error() = %v, want %v", err.Error(), "example error [{\"param1\":\"value1\",\"param2\":\"value2\"}]")
@@ -30,7 +30,7 @@ func Test_ConstErrorParams(t *testing.T) {
 }
 
 func TestConstErrorWrappedParams(t *testing.T) {
-	params := map[string]string{"param1": "value1", "param2": "value2"}
+	params := map[string]any{"param1": "value1", "param2": "value2"}
 	err := exampleError.WrappedWithParams(wrappedError, params)
 	if err.Error() != "example error: wrapped error [{\"param1\":\"value1\",\"param2\":\"value2\"}]" {
 		t.Errorf(
